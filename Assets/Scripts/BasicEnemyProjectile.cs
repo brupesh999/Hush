5ed16d3c3902386fa8838dd8f7579ba625143438
TheMuffinPerson <dslavin@mit.edu>
2025-11-15 19:30:03 -0500

    enemy projectiles deal dmg
    player now has HP, starts at 10. enemy projectile deals 1 dmg. Projectiles seem to apply force to player, may need fixing

--- Assets/Scripts/BasicEnemyProjectile.cs
+++ Assets/Scripts/BasicEnemyProjectile.cs
@@ -4,10 +4,7 @@ public class BasicEnemyProjectile : MonoBehaviour
 {
 
     [SerializeField] private float velocity = -5f;
-
-    // void Awake(){
-    //     renderer = GetComponent<Renderer>();
-    // }
+    public float damage = 1f;//needs to be public so hit player can access it
 
     // Update is called once per frame
     void Update()
@@ -16,6 +13,13 @@ public class BasicEnemyProjectile : MonoBehaviour
 
         if (!GetComponent<Renderer>().isVisible){
             //if the projectile is no longer on-screen, destroy it
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+        //when it hits the player, disappear
+        if (other.gameObject.tag == "Player"){
             Destroy(gameObject);
         }
     }
