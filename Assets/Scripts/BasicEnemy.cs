1e90ab61bc6f4aaa8a56d7e9815c7c8ba8364bcf
TheMuffinPerson <dslavin@mit.edu>
2025-11-15 21:54:46 -0500

    enemy only shoots at player in radius
    added cast (from collider) that will only shoot if it detects a Player w/in a certain distance. No movement yet

--- Assets/Scripts/BasicEnemy.cs
+++ Assets/Scripts/BasicEnemy.cs
@@ -1,25 +1,35 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class BasicEnemy : MonoBehaviour
 {
     float timekeeper = 0f;//this will count how much time has passed (in s) since enemy has shot a projectile
     [SerializeField] private GameObject projectile;
-    [SerializeField] private float shootInterval;
+    [SerializeField] private float shootInterval = 2f;
+    [SerializeField] private float detectionDistance = 7f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private List<RaycastHit2D> castResult = new List<RaycastHit2D>();
 
     // Update is called once per frame
     void Update()
     {
         timekeeper += Time.deltaTime;//increase timer every update
         
-        if (timekeeper >= shootInterval){
-            timekeeper = 0f;
-            Instantiate(projectile, transform.position + new Vector3(-0.5f, 0, 0), Quaternion.identity);
+        if (gameObject.GetComponent<Collider2D>().Cast(new Vector2(-1, 0), castResult, detectionDistance) > 0){
+
+            bool foundPlayer = false;
+            foreach (RaycastHit2D hitItem in castResult){
+                if (hitItem.transform.gameObject.tag == "Player"){
+                    foundPlayer = true;
+                    break;
+                }
+            }
+
+            if (foundPlayer && timekeeper >= shootInterval){
+                timekeeper = 0f;
+                Instantiate(projectile, transform.position + new Vector3(-0.5f, 0, 0), Quaternion.identity);
+            }
         }
     }
 }
